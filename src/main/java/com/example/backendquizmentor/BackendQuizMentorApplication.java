36d886ab284865d8ceb18a5d3f1dedaa339c95f0
olyshaaa <olgasevel108@gmail.com>
2023-12-05 16:47:04 +0200

    login and register

---
+++ src/main/java/com/example/backendquizmentor/BackendQuizMentorApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendquizmentor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendQuizMentorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendQuizMentorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendquizmentor/BackendQuizMentorApplicationTests.java
@@ -0,0 +1,10 @@
+package com.example.backendquizmentor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendQuizMentorApplicationTests {
+
+    @Test
+    void contextLoads() {
